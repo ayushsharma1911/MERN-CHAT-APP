c72cdf224f195f6b4a0017eaac9fdddd2900f7ad
Ayush <ayushayushayush007@gmail.com>
2022-07-03 04:15:30 +0530

    changes

---
+++ Backend/index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const chats = require("./data"); 
+const dotenv= require("dotenv");
+const connectDB = require ("./config/db"); 
+const userRoutes= require('./routes/userRoutes');
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const chatRoutes= require('./routes/chatRoutes');
+const messageRoutes= require('./routes/messageRoutes')
+const app= express();
+dotenv.config();
+connectDB();
+
+app.use(express.json()); // telling the fe to use the json
+
+app.get('/',(req,res)=>{
+    res.send("API is running");
+});
+
+app.use('/api/user',userRoutes);
+app.use('/api/chat', chatRoutes);
+app.use("/api/message", messageRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const server=app.listen(process.env.PORT||3000,console.log("Server is up at port 3000"));
+
+const io= require('socket.io')(server,{
+    //pingtimeout -> the amount of time it will wait before being inactive
+    pingTimeout:60000,
+    //cors to neglet cross origin error
+    cors:{
+        origin:"http://localhost:3001", // the port on which FE is running
+    },
+})
+
+io.on("connection",(socket)=>{
+    console.log('connected to socket.io');
+
+    //creating a new socket where fe will send some data and join our room
+    socket.on("setup", (userData) => {
+        socket.join(userData._id);
+        console.log(userData._id);
+        socket.emit("connected");});
+
+        //joining a chat room, the two user will join the same room
+        socket.on("join chat", (room) => {
+            socket.join(room);
+            console.log("User Joined Room: " + room);
+          });
+
+        //new message
+        socket.on("new message", (newMessageRecieved) => {
+            var chat = newMessageRecieved.chat;
+        
+            if (!chat.users) return console.log("chat.users not defined");
+        
+            // the message should be sent to only recievers in the grp
+            chat.users.forEach((user) => {
+              if (user._id == newMessageRecieved.sender._id) return;
+        
+              socket.in(user._id).emit("message recieved", newMessageRecieved); //in <- inside that message room
+            });
+          });
+
+});
---
+++ Backend/routes/messageRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {
+  allMessages,
+  sendMessage,
+} = require("../controllers/messageControllers.js");
+const { protect } = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+// get all the message
+router.route("/:chatId").get(protect, allMessages);
+
+//send message
+router.route("/").post(protect, sendMessage);
+
+module.exports = router;
